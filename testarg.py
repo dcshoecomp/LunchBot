4372ddbda725742b3145f4dfb6de416c6d04798b
J L <jeonnlem@gmail.com>
2019-09-04 13:10:30 +0000

    Updated testarg.py

--- testarg.py
+++ testarg.py
@@ -1,2 +1,3 @@
 import sys
-print(sys.argv)
+url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={1}&radius={2}type=restaurant&fields=name&key={0}'.format(sys.argv[1],sys.argv[3],sys.argv[4])
+print(url)
